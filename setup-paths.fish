5d7dccc5658869272e1db9b3b941bc4c7e4382a4
Abeer Vaishnav <abeer.vaishnav13@gmail.com>
2023-04-19 18:13:57 -0400

    Added script for linking all files in new system

---
+++ setup-paths.fish
@@ -0,0 +1,10 @@
+#!/opt/homebrew/bin/fish
+
+ln -Fs ~/Documents/Coding/config-manager/alacritty ~/.config
+ln -Fs ~/Documents/Coding/config-manager/bat ~/.config
+ln -Fs ~/Documents/Coding/config-manager/fish ~/.config
+ln -Fs ~/Documents/Coding/config-manager/helix ~/.config
+ln -Fs ~/Documents/Coding/config-manager/lazygit ~/.config
+ln -Fs ~/Documents/Coding/config-manager/lvim ~/.config
+ln -Fs ~/Documents/Coding/config-manager/.warp ~/
+ln -Fs ~/Documents/Coding/config-manager/zellij ~/.config
