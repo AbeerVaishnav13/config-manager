8f6e3762963c8e36e73e6f6829d294e01df81275
Abeer Vaishnav <abeer.vaishnav13@gmail.com>
2023-04-19 18:54:54 -0400

    Fix: change to relative path instead of absolute path

--- install-dev-setup-paths.fish
+++ install-dev-setup-paths.fish
@@ -2,7 +2,7 @@
 
 ####### Install packages #######
 # Install necessary packages 
-/opt/homebrew/bin/brew install neovim python@3.10 gcc bat exa zellij cmake git lazygit make pandoc stylua bpytop latexindent marksman par ripgrep yaml-language-server
+# /opt/homebrew/bin/brew install neovim python@3.10 gcc bat exa zellij cmake git lazygit make pandoc stylua bpytop latexindent marksman par ripgrep yaml-language-server
 
 # Install optional packages
 # /opt/homebrew/bin/brew install rust rust-analyzer helix marp-cli
@@ -11,16 +11,16 @@
 # /opt/homebrew/bin/brew --cask install alacritty mactex klayout parraview zoom discord anaconda brave-browser xquartz slack amethyst keka elmedia-player git-delta
 
 ####### Setup Config files #######
-mkdir -p ~/.config
+# mkdir -p ~/.config
 
 # Setup necessary configs
-ln -Fs ~/Documents/Coding/config-manager/alacritty ~/.config
-ln -Fs ~/Documents/Coding/config-manager/bat ~/.config
-ln -Fs ~/Documents/Coding/config-manager/fish ~/.config
-ln -Fs ~/Documents/Coding/config-manager/lazygit ~/.config
-ln -Fs ~/Documents/Coding/config-manager/lvim ~/.config
-ln -Fs ~/Documents/Coding/config-manager/.warp ~/
-ln -Fs ~/Documents/Coding/config-manager/zellij ~/.config
+ln -Fs $PWD/alacritty ~/.config
+ln -Fs $PWD/bat ~/.config
+ln -Fs $PWD/fish ~/.config
+ln -Fs $PWD/lazygit ~/.config
+ln -Fs $PWD/lvim ~/.config
+ln -Fs $PWD/.warp ~/
+ln -Fs $PWD/zellij ~/.config
 
 # Setup optional configs
 # ln -Fs ~/Documents/Coding/config-manager/helix ~/.config
