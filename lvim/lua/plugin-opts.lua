2ca5abe852ccd4c480bb71b64ae340ed1ad590cb
Abeer Vaishnav <abeer.vaishnav13@gmail.com>
2023-04-19 20:28:36 -0400

    Remove ts-playground as nvim 0.9 has InspectTree

--- lvim/lua/plugin-opts.lua
+++ lvim/lua/plugin-opts.lua
@@ -42,33 +42,6 @@ lvim.plugins = {
 	{
 		"dag/vim-fish",
 	},
-	-- {
-	-- 	"nvim-treesitter/playground",
-	-- 	config = function()
-	-- 		-- TS Playground setup
-	-- 		local ts_playground_opts = {
-	-- 			playground = {
-	-- 				enable = true,
-	-- 				disable = {},
-	-- 				updatetime = 25, -- Debounced time for highlighting nodes in the playground from source code
-	-- 				persist_queries = false, -- Whether the query persists across vim sessions
-	-- 				keybindings = {
-	-- 					toggle_query_editor = "o",
-	-- 					toggle_hl_groups = "i",
-	-- 					toggle_injected_languages = "t",
-	-- 					toggle_anonymous_nodes = "a",
-	-- 					toggle_language_display = "I",
-	-- 					focus_language = "f",
-	-- 					unfocus_language = "F",
-	-- 					update = "R",
-	-- 					goto_node = "<cr>",
-	-- 					show_help = "?",
-	-- 				},
-	-- 			},
-	-- 		}
-	-- 		require("nvim-treesitter.configs").setup(ts_playground_opts)
-	-- 	end,
-	-- },
 	{
 		"ziontee113/icon-picker.nvim",
 		dependencies = "stevearc/dressing.nvim",
