8bacdf585af40341d7d8985e69de5455849fe6b1
Abeer Vaishnav <abeer.vaishnav13@gmail.com>
2023-05-18 22:16:25 -0400

    Update install script

--- install-dev-setup-paths.fish
+++ install-dev-setup-paths.fish
@@ -5,22 +5,23 @@
 # /opt/homebrew/bin/brew install neovim python@3.10 node gcc bat exa zellij cmake git lazygit make pandoc stylua bpytop latexindent marksman par ripgrep yaml-language-server fd
 
 # Install optional packages
-# /opt/homebrew/bin/brew install rust rust-analyzer helix marp-cli
+# /opt/homebrew/bin/brew install rust rust-analyzer helix marp-cli alacritty
 
 ####### Install casks #######
-# /opt/homebrew/bin/brew --cask install alacritty basictex klayout parraview zoom discord anaconda brave-browser xquartz slack amethyst keka elmedia-player git-delta
+# /opt/homebrew/bin/brew --cask install wezterm basictex klayout paraview zoom discord anaconda brave-browser xquartz slack amethyst keka elmedia-player git-delta
 
 ####### Setup Config files #######
 # mkdir -p ~/.config
 
 # Setup necessary configs
-ln -Fs $PWD/alacritty ~/.config
+ln -Fs $PWD/.warp ~/
 ln -Fs $PWD/bat ~/.config
 ln -Fs $PWD/fish ~/.config
 ln -Fs $PWD/lazygit ~/.config
 ln -Fs $PWD/lvim ~/.config
-ln -Fs $PWD/.warp ~/
+ln -Fs $PWD/wezterm ~/.config
 ln -Fs $PWD/zellij ~/.config
 
 # Setup optional configs
+# ln -Fs $PWD/alacritty ~/.config
 # ln -Fs ~/Documents/Coding/config-manager/helix ~/.config
