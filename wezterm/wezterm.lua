424fac8d18372dbcf3f7f9fc1aab8d1f22539a34
Abeer Vaishnav <abeer.vaishnav13@gmail.com>
2023-05-19 13:09:15 -0400

    Added keymaps for multiplexing

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -14,4 +14,62 @@ config.line_height = 1.2
 config.window_padding = { left = 0, right = 0, top = 0, bottom = 0 }
 config.quit_when_all_windows_are_closed = true
 
+config.keys = {
+	{
+		key = "w",
+		mods = "CMD",
+		action = wezterm.action.CloseCurrentPane({ confirm = true }),
+	},
+	{
+		key = "v",
+		mods = "CMD|SHIFT",
+		action = wezterm.action.SplitVertical({ domain = "CurrentPaneDomain" }),
+	},
+	{
+		key = "p",
+		mods = "CMD|SHIFT",
+		action = wezterm.action.SplitHorizontal({ domain = "CurrentPaneDomain" }),
+	},
+	{
+		key = "z",
+		mods = "CMD|SHIFT",
+		action = wezterm.action.TogglePaneZoomState,
+	},
+	{
+		key = "h",
+		mods = "CMD|CTRL",
+		action = wezterm.action.RotatePanes("CounterClockwise"),
+	},
+	{
+		key = "k",
+		mods = "CMD|CTRL",
+		action = wezterm.action.RotatePanes("Clockwise"),
+	},
+	{
+		key = "h",
+		mods = "CMD|SHIFT",
+		action = wezterm.action.ActivatePaneDirection("Left"),
+	},
+	{
+		key = "k",
+		mods = "CMD|SHIFT",
+		action = wezterm.action.ActivatePaneDirection("Right"),
+	},
+	{
+		key = "u",
+		mods = "CMD|SHIFT",
+		action = wezterm.action.ActivatePaneDirection("Up"),
+	},
+	{
+		key = "j",
+		mods = "CMD|SHIFT",
+		action = wezterm.action.ActivatePaneDirection("Down"),
+	},
+	{
+		key = " ",
+		mods = "CTRL",
+		action = wezterm.action.ActivateCommandPalette,
+	},
+}
+
 return config
