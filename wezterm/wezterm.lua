561b897616d805574ad641febb34c68a826be2d3
Abeer Vaishnav <abeer.vaishnav13@gmail.com>
2023-05-20 21:16:33 -0400

    Rearrage function args

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -27,10 +27,10 @@ config.quit_when_all_windows_are_closed = true
 config.scrollback_lines = 100000
 
 -- Keymaps
-local map_key = function(key, mods, act_id, opts)
+local map_key = function(mods, key, act_id, opts)
 	local keymap = {}
-	keymap.key = key
 	keymap.mods = mods
+	keymap.key = key
 
 	local action_obj = wezterm.action[act_id]
 	if type(action_obj) == "string" then
@@ -43,17 +43,17 @@ local map_key = function(key, mods, act_id, opts)
 end
 
 config.keys = {
-	map_key("w", "CMD", "CloseCurrentPane", { confirm = true }),
-	map_key("v", "CMD|SHIFT", "SplitVertical", { domain = "CurrentPaneDomain" }),
-	map_key("s", "CMD|SHIFT", "SplitHorizontal", { domain = "CurrentPaneDomain" }),
-	map_key("h", "CMD|CTRL", "RotatePanes", "CounterClockwise"),
-	map_key("k", "CMD|CTRL", "RotatePanes", "Clockwise"),
-	map_key("h", "CMD|SHIFT", "ActivatePaneDirection", "Left"),
-	map_key("k", "CMD|SHIFT", "ActivatePaneDirection", "Right"),
-	map_key("u", "CMD|SHIFT", "ActivatePaneDirection", "Up"),
-	map_key("j", "CMD|SHIFT", "ActivatePaneDirection", "Down"),
-	map_key("z", "CMD|SHIFT", "TogglePaneZoomState"),
-	map_key(" ", "CTRL", "ActivateCommandPalette"),
+	map_key("CMD", "w", "CloseCurrentPane", { confirm = true }),
+	map_key("CMD|SHIFT", "v", "SplitVertical", { domain = "CurrentPaneDomain" }),
+	map_key("CMD|SHIFT", "s", "SplitHorizontal", { domain = "CurrentPaneDomain" }),
+	map_key("CMD|CTRL", "h", "RotatePanes", "CounterClockwise"),
+	map_key("CMD|CTRL", "k", "RotatePanes", "Clockwise"),
+	map_key("CMD|SHIFT", "h", "ActivatePaneDirection", "Left"),
+	map_key("CMD|SHIFT", "k", "ActivatePaneDirection", "Right"),
+	map_key("CMD|SHIFT", "u", "ActivatePaneDirection", "Up"),
+	map_key("CMD|SHIFT", "j", "ActivatePaneDirection", "Down"),
+	map_key("CMD|SHIFT", "z", "TogglePaneZoomState"),
+	map_key("CTRL", " ", "ActivateCommandPalette"),
 }
 
 return config
