3afc395f215467b1f3b87cf8c68ed77f3a1a701f
Abeer Vaishnav <abeer@atlantic-quantum.com>
2023-06-02 16:22:16 -0400

    Multiple changes
    - Added GitHub copilot
    - Changes from `yapf` to `black` for formatting

--- lvim/lua/plugin-opts.lua
+++ lvim/lua/plugin-opts.lua
@@ -106,26 +106,8 @@ lvim.plugins = {
 		end,
 	},
 	{
-		"Exafunction/codeium.vim",
-		config = function()
-			vim.g.codeium_enabled = false
-			vim.g.codeium_disable_bindings = 1
-			vim.keymap.set("i", "<C-l>", function()
-				return vim.fn["codeium#Accept"]()
-			end, { expr = true })
-			vim.keymap.set("i", "<C-]>", function()
-				return vim.fn["codeium#CycleCompletions"](1)
-			end, { expr = true })
-			vim.keymap.set("i", "<C-[>", function()
-				return vim.fn["codeium#CycleCompletions"](-1)
-			end, { expr = true })
-			vim.keymap.set("i", "<C-x>", function()
-				return vim.fn["codeium#Clear"]()
-			end, { expr = true })
-			vim.keymap.set("i", "<C-s>", function()
-				return vim.fn["codeium#Complete"]()
-			end, { expr = true })
-		end,
+		"github/copilot.vim",
+		event = "InsertEnter",
 	},
 }
 
@@ -179,7 +161,7 @@ formatters.setup({
 	{ command = "latexindent", filetypes = { "tex" } },
 	{ command = "rustfmt", filetypes = { "rust" } },
 	{ command = "stylua", filetypes = { "lua" } },
-	{ command = "yapf", filetypes = { "python" } },
+	{ command = "black", filetypes = { "python" } },
 	-- { command = "yamlfmt", filetypes = { "yaml" } },
 	{ command = "zigfmt", filetypes = { "zig" } },
 })
